bed52e493ed82188a9e9b2c2380ddf1dfe622ebb
mvrlrd <igrbelykh@gmail.com>
2020-03-14 21:44:39 +0300

    cleaned the code

--- app/src/main/java/ru/mvrlrd/lesson2/task2/Observable.java
+++
@@ -1,7 +0,0 @@
-package ru.mvrlrd.lesson2.task2;
-
-public interface Observable {
-    void subscribeObserver(Observer observer);
-    void unsubscribeObserver(Observer observer);
-    void notifyAllObservers();
-}
--- app/src/main/java/ru/mvrlrd/lesson2/task2/Observer.java
+++
@@ -1,5 +0,0 @@
-package ru.mvrlrd.lesson2.task2;
-
-public interface Observer {
-    void updateObserver(boolean flag);
-}
